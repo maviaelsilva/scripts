39211a8f664218039848a8683aa8240b0f64b905
Maviael Silva <maviael.silva@gmail.com>
2017-08-16 12:30:58 -0300

    Ajuste MASK

--- bgp_prefix_by_asn.sh
+++ bgp_prefix_by_asn.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 ASN=$1
-whois -h whois.registro.br AS$ASN | awk 'match($0,/((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/) {print substr($0,RSTART,RLENGTH)}' > /tmp/bgp_$ASN
+whois -h whois.registro.br AS$ASN | awk 'match($0,/(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))/) {print substr($0,RSTART,RLENGTH)}' > /tmp/bgp_$ASN
 echo "ASN: $ASN"
 cat  /tmp/bgp_$ASN | while read LINE
 do
-  echo "prefix: $LINE"
-  PREFIX=$LINE
+  PREFIX=`echo $LINE | cut -d'/' -f 1`
+  MASK=`echo $LINE | cut -d'/' -f 2`
+  echo "Prefix: $LINE (Netowrk: $PREFIX, Mask: $MASK)"
   echo "LG SEABONE - AS-PATH"
   curl -s -k 'https://gambadilegno.noc.seabone.net/lg/lg.cgi' \
   -XPOST \
@@ -27,7 +28,7 @@ do
   -H 'Referer: http://lg.level3.net/bgp/lg_bgp_output.php' \
   -H 'Upgrade-Insecure-Requests: 1' \
   -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
-  -H 'Origin: http://lg.level3.net' --data "sitename=ear1.ams1&address=$PREFIX&length=22" \
+  -H 'Origin: http://lg.level3.net' --data "sitename=ear1.ams1&address=$PREFIX&length=$MASK" \
   -o /tmp/level3-$PREFIX.html
 
    cat /tmp/level3-$PREFIX.html | eval "awk 'match(\$0,/[0-9 ]+ "$ASN"/) {print substr(\$0,RSTART,RLENGTH)}'" | uniq
