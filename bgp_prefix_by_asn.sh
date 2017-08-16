9b0fc281e2665a7bba8747224743feafb3190fbc
Maviael Silva <maviael.silva@gmail.com>
2017-08-16 00:11:46 -0300

    first release

---
+++ bgp_prefix_by_asn.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ASN=$1
+whois -h whois.registro.br AS$ASN | awk 'match($0,/((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/) {print substr($0,RSTART,RLENGTH)}' > /tmp/bgp_$ASN
+echo "ASN: $ASN"
+cat  /tmp/bgp_$ASN | while read LINE
+do
+  echo "prefix: $LINE"
+  PREFIX=$LINE
+  echo "LG SEABONE - AS-PATH"
+  curl -s -k 'https://gambadilegno.noc.seabone.net/lg/lg.cgi' \
+  -XPOST \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Referer: https://gambadilegno.noc.seabone.net/lg/' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
+  -H 'Origin: https://gambadilegno.noc.seabone.net' \
+  --data "query=bgp&addr=${PREFIX}&router=RioDeJaneiro" \
+  -o /tmp/seabone-$PREFIX.html
+
+  cat /tmp/seabone-$PREFIX.html | eval "awk 'match(\$0,/[0-9 ]+ "$ASN"/) {print substr(\$0,RSTART,RLENGTH)}'" | uniq
+
+  echo ""
+
+  echo "LG LEVEL3 - AS-PATH"
+  curl -s 'http://lg.level3.net/bgp/lg_bgp_output.php' \
+  -XPOST -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Referer: http://lg.level3.net/bgp/lg_bgp_output.php' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' \
+  -H 'Origin: http://lg.level3.net' --data "sitename=ear1.ams1&address=$PREFIX&length=22" \
+  -o /tmp/level3-$PREFIX.html
+
+   cat /tmp/level3-$PREFIX.html | eval "awk 'match(\$0,/[0-9 ]+ "$ASN"/) {print substr(\$0,RSTART,RLENGTH)}'" | uniq
+  echo ""
+  echo ""
+
+done
